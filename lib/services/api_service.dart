c405eeb2258b444d3a5380b2a2143c80dd87241f
XTerris <redunicorn94@gmail.com>
2025-09-14 14:06:16 +0300

    initial commit

---
+++ lib/services/api_service.dart
@@ -0,0 +1,7 @@
+class ApiService {
+  // TODO: Implement REST API methods
+  Future<Map<String, dynamic>> get(String endpoint) async {
+    // Placeholder for GET request
+    return {};
+  }
+}
---
+++ lib/services/hive_service.dart
@@ -0,0 +1,11 @@
+class HiveService {
+  // TODO: Implement Hive DB methods
+  Future<void> save(String box, dynamic data) async {
+    // Placeholder for save method
+  }
+
+  Future<dynamic> get(String box, String key) async {
+    // Placeholder for get method
+    return null;
+  }
+}
